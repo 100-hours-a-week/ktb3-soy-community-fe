e13ca77c46df340237c18b9914fb87e32cf52374
soykeepgoing <soykeepgoing@gmail.com>
2025-11-09 12:30:09 +0900

    fix: 게시글 조회 포맷 설정

--- posts.js
+++ posts.js
@@ -1,5 +1,11 @@
 const params = new URLSearchParams(window.location.search);
 const userId = params.get("userId");
+const src_default_user_profile = "../images/default_user_profile.png";
+
+function fromCreatedAt(createdAt){
+  return createdAt.replace("T", " ")
+}
+
 
 function renderPosts(posts){
     const postList = document.getElementById("post-list");
@@ -8,12 +14,16 @@ function renderPosts(posts){
         postCard.classList.add("post-card");
         postCard.innerHTML = `
         <h3>${post.title}</h3>
-        <div class="meta">
-            <span>좋아요 ${post.statsLikeCounts} 댓글 ${post.statsCommentCounts} 조회수 ${post.statsViewCounts}</span>
-            <span>${post.createdAt}</span>
+        <div class="post-meta">
+          <p>좋아요 ${post.statsLikeCounts} 댓글 ${post.statsCommentCounts} 조회수 ${post.statsViewCounts}</p>
+          <p>${fromCreatedAt(post.createdAt)}</p>
         </div>
+        <hr/>
         <div class="author">
-            <span>${post.userNickname}</span>
+          <img class="profile-img" id="profile-img" src=${post.userProfileImgUrl}
+          onerror="this.onerror=null; this.src='${src_default_user_profile}';"
+          />
+          <span>${post.userNickname}</span>
         </div>
         `;
         postList.appendChild(postCard);
@@ -21,10 +31,9 @@ function renderPosts(posts){
 }
 
 
-fetch(`http://localhost:8080/api/posts?page=2&size=2`)
+fetch(`http://localhost:8080/api/posts?page=0&size=20`)
   .then(response => response.json())
   .then(data => {
-    console.log(data.postItemResponseList);
     renderPosts(data.postItemResponseList);
   })
 .catch(err => console.error(err));
