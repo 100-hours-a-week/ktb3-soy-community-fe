833cc4c80c51f26dd53a067666023b77c8675328
soykeepgoing <soykeepgoing@gmail.com>
2025-11-09 16:36:04 +0900

    feat: 게시글 무한 스크롤 기능 추가

--- posts.js
+++ posts.js
@@ -1,21 +1,38 @@
-const params = new URLSearchParams(window.location.search);
-const userId = params.get("userId");
 const src_default_user_profile = "../images/default_user_profile.png";
+const getPostsURL = "http://localhost:8080/api/posts?";
+const default_size = 20;
+let currentPage = 0;
+let noMorePage = false;
+let postList = document.getElementById('post-list');
 
 function fromCreatedAt(createdAt){
   return createdAt.replace("T", " ")
 }
 
+function getTitle(title){
+  return title.substr(0, 27);
+}
+
+function getCount(count){
+  if (count >= 100000) return "100K";
+  else if(count >= 10000) return "10K";
+  else if(count >= 1000) return "1K";
+  else return count;
+}
 
 function renderPosts(posts){
-    const postList = document.getElementById("post-list");
     posts.forEach(post => {
+        const title = getTitle(post.title);
+        const likeCount = getCount(post.statsLikeCounts);
+        const commentCount = getCount(post.statsCommentCounts);
+        const viewCount = getCount(post.statsViewCounts);
+
         const postCard = document.createElement("div");
         postCard.classList.add("post-card");
         postCard.innerHTML = `
-        <h3>${post.title}</h3>
+        <h3>${title}</h3>
         <div class="post-meta">
-          <p>좋아요 ${post.statsLikeCounts} 댓글 ${post.statsCommentCounts} 조회수 ${post.statsViewCounts}</p>
+          <p>좋아요 ${likeCount} 댓글 ${commentCount} 조회수 ${viewCount}</p>
           <p>${fromCreatedAt(post.createdAt)}</p>
         </div>
         <hr/>
@@ -31,9 +48,41 @@ function renderPosts(posts){
 }
 
 
-fetch(`http://localhost:8080/api/posts?page=0&size=20`)
-  .then(response => response.json())
-  .then(data => {
-    renderPosts(data.postItemResponseList);
-  })
-.catch(err => console.error(err));
+
+function fetchPosts(){
+  fetch(getPostsURL + `page=${currentPage}&size=${default_size}`).then(
+    response => response.json()
+  ).then(
+    data => {
+      renderPosts(data.postItemResponseList);
+      if (data.pagingMeta.pageSize === 0){
+        noMorePage = true;
+      }
+    }
+  ).catch(
+    err => console.error("게시글 목록 조회 실패", err));
+  currentPage++;
+}
+
+fetchPosts();
+
+
+window.addEventListener('scroll', () => {
+  if (noMorePage) return;
+
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    fetchPosts();
+  }
+
+  const writeBtn = document.querySelector(".button");
+  const floatBtn = document.querySelector(".float-button");
+
+  const rect = writeBtn.getBoundingClientRect();
+  
+  if (rect.bottom < 0) {
+    floatBtn.classList.add("show");
+  } else {
+    floatBtn.classList.remove("show");
+  }
+});
+
